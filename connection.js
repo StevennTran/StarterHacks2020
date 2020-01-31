0bc701ca3b8f6425785da52ad2af1e8461ec2944
sri13.vinukonda@gmail.com <>
2020-01-31 12:13:04 -0500

    adjusted css

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 //Connect to mongodb
-mongoose.connect('mongodb://localhost/citiesclear');
+mongoose.connect('mongodb://localhost/cities');
 
 mongoose.connection.once('open',function() {
     console.log('connection made, now make fire');
